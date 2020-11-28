546078d9d8a5a500516f307d25ce15ef5b65b8a5
SaxophonUser <saxophonuser@web.de>
2020-11-28 12:57:19 +0100

    further implementation of logic

--- main.js
+++ main.js
@@ -11,6 +11,9 @@ const utils = require('@iobroker/adapter-core');
 // Load your modules here, e.g.:
 // const fs = require("fs");
 
+/** @type {number | undefined} */
+let rainQuantity;
+
 class RainMeter extends utils.Adapter {
 
     /**
@@ -36,53 +39,126 @@ class RainMeter extends utils.Adapter {
 
         // The adapters config (in the instance object everything under the attribute "native") is accessible via
         // this.config:
-        this.log.info('config option1: ' + this.config.option1);
-        this.log.info('config option2: ' + this.config.option2);
+        this.log.info('config triggerVariable: ' + this.config.triggerVariable);
+        this.log.info('config rainQuantity: ' + this.config.rainQuantity);
+
+        rainQuantity = parseFloat(this.config.rainQuantity);
+
+        this.log.info('rainQuantity as float: ' + rainQuantity);
 
         /*
         For every state in the system there has to be also an object of type state
         Here a simple template for a boolean variable named "testVariable"
         Because every adapter instance uses its own unique namespace variable names can't collide with other adapters variables
         */
-        await this.setObjectNotExistsAsync('testVariable', {
-            type: 'state',
-            common: {
-                name: 'testVariable',
-                type: 'boolean',
-                role: 'indicator',
-                read: true,
-                write: true,
-            },
-            native: {},
-        });
 
         // In order to get state updates, you need to subscribe to them. The following line adds a subscription for our variable we have created above.
-        this.subscribeStates('testVariable');
+
         // You can also add a subscription for multiple states. The following line watches all states starting with "lights."
         // this.subscribeStates('lights.*');
         // Or, if you really must, you can also watch all states. Don't do this if you don't need to. Otherwise this will cause a lot of unnecessary load on the system:
         // this.subscribeStates('*');
+        this.subscribeForeignStates(this.config.triggerVariable); // subscribe on variable "forecast.html" of all adapter instances "yr".
+
+        this.createStates();
 
         /*
             setState examples
             you will notice that each setState will cause the stateChange event to fire (because of above subscribeStates cmd)
         */
         // the variable testVariable is set to true as command (ack=false)
-        await this.setStateAsync('testVariable', true);
 
         // same thing, but the value is flagged "ack"
         // ack should be always set to true if the value is received from or acknowledged from the target system
-        await this.setStateAsync('testVariable', { val: true, ack: true });
 
         // same thing, but the state is deleted after 30s (getState will return null afterwards)
-        await this.setStateAsync('testVariable', { val: true, ack: true, expire: 30 });
 
         // examples for the checkPassword/checkGroup functions
-        let result = await this.checkPasswordAsync('admin', 'iobroker');
-        this.log.info('check user admin pw iobroker: ' + result);
+        // let result = await this.checkPasswordAsync('admin', 'iobroker');
+        // this.log.info('check user admin pw iobroker: ' + result);
 
-        result = await this.checkGroupAsync('admin', 'admin');
-        this.log.info('check group user admin group admin: ' + result);
+        // result = await this.checkGroupAsync('admin', 'admin');
+        // this.log.info('check group user admin group admin: ' + result);
+    }
+
+    async createStates()
+    {
+        var thisHelper = this;
+
+        await this.createNewState('rainQuantity');
+        await this.createNewState('numberOfInputSignals');
+        await this.createNewState('year');
+        await this.createNewState('quarter');
+        await this.createNewState('month');
+        await this.createNewState('week');
+        await this.createNewState('day');
+        await this.createNewState('hour');
+        await this.createNewState('yesterday');
+        await this.createNewState('dayBeforeYesterday');
+
+        this.setValue('rainQuantity', 0.0);
+        this.setValue('numberOfInputSignals', 0);
+        this.setValue('year', 0.0);
+        this.setValue('quarter', 0.0);
+        this.setValue('month', 0.0);
+        this.setValue('week', 0.0);
+        this.setValue('day', 0.0);
+        this.setValue('hour', 0.0);
+        this.setValue('yesterday', 0.0);
+        this.setValue('dayBeforeYesterday', 0.0);
+    }
+
+    async createNewState(name)
+    {
+        await this.setObjectNotExistsAsync(name, {
+            type: 'state',
+            common: {
+                name: name,
+                type: 'number',
+                role: 'indicator',
+                read: true,
+                write: true,
+            },
+            native: {},
+        });
+    }
+
+    async setValue(name, newValue)
+    {
+        var thisHelper = this;
+        this.getStateAsync(name, function (err, value) {
+            if (value == null) {
+                thisHelper.log.info(name + " is null, set it to " + newValue);
+                thisHelper.setStateAsync(name, newValue);
+            }
+            else
+                thisHelper.log.info(name + " value = " + value.val);
+        });
+    }
+
+    async increment(name)
+    {
+        var thisHelper = this;
+        this.getStateAsync(name, function (err, value) {
+            if (value == null) {
+                thisHelper.setStateAsync(name, 0);
+            }
+            else {
+                var newValue = Math.round(100 * (value.val  + rainQuantity )) / 100
+                thisHelper.setStateAsync(name, newValue);
+            }
+        });
+    }
+
+    incrementAll() {
+        this.increment('rainQuantity');
+        this.increment('year');
+        this.increment('quarter');
+        this.increment('month');
+        this.increment('week');
+        this.increment('day');
+        this.increment('yesterday');
+        this.increment('dayBeforeYesterday');
     }
 
     /**
@@ -127,7 +203,43 @@ class RainMeter extends utils.Adapter {
      */
     onStateChange(id, state) {
         if (state) {
+            var help = this;
             // The state was changed
+            if (id == this.config.triggerVariable)
+            {
+                // this.getState('rainQuantity', function (err, quantity) {
+                //
+                //     help.log.info('old value = ' + quantity.val);
+                //     var value = quantity.val + rainQuantity;
+                //     help.log.info('new value = ' + value);
+                //     help.setStateAsync('rainQuantity', Number(value.toFixed(2)));
+                //
+                //     help.log.info(
+                //         '  Value: '        + quantity.val +
+                //         ', ack: '          + quantity.ack +
+                //         ', time stamp: '   + quantity.ts  +
+                //         ', last changed: ' + quantity.lc
+                //     );
+                // });
+                this.incrementAll();
+                this.getState('numberOfInputSignals', function (err, counter) {
+
+                    help.log.info('old value = ' + counter.val);
+                    var value = counter.val + 1;
+                    help.log.info('new value = ' + value);
+                    help.setStateAsync('numberOfInputSignals', Number(value.toFixed(2)));
+
+                    help.log.info(
+                        '  Value: '        + counter.val +
+                        ', ack: '          + counter.ack +
+                        ', time stamp: '   + counter.ts  +
+                        ', last changed: ' + counter.lc
+                    );
+                });
+
+
+            }
+
             this.log.info(`state ${id} changed: ${state.val} (ack = ${state.ack})`);
         } else {
             // The state was deleted
